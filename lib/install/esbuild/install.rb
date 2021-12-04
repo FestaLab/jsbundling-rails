83c5328ee4a2cb8dd879c0c290da2530668aedeb
Breno Gazzola <breno.gazzola@gmail.com>
2021-12-04 12:57:06 -0300

    Add config file for esbuild

--- lib/install/esbuild/install.rb
+++ lib/install/esbuild/install.rb
@@ -1,8 +1,9 @@
-say "Install esbuild"
+say "Install esbuild with config"
+copy_file "#{__dir__}/esbuild.config.js", "esbuild.config.js"
 run "yarn add esbuild"
 
 say "Add build script"
-build_script = "esbuild app/javascript/*.* --bundle --sourcemap --outdir=app/assets/builds"
+build_script = "node esbuild.config.js"
 
 if (`npx -v`.to_f < 7.1 rescue "Missing")
   say %(Add "scripts": { "build": "#{build_script}" } to your package.json), :green
